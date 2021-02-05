698a023a9aedc9704726526e32e5cecd70b005f2
POKRZYWA Dominique <dominique.pokrzywa@axa.fr>
2021-02-05 16:27:07 +0100

    getUserReference Ok

--- src/main/java/tourGuide/service/TourGuideService.java
+++ src/main/java/tourGuide/service/TourGuideService.java
@@ -1,37 +1,44 @@
 package tourGuide.service;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import org.apache.commons.lang3.time.StopWatch;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
 import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
 import gpsUtil.location.Location;
 import gpsUtil.location.VisitedLocation;
-
+import org.apache.commons.lang3.time.StopWatch;
+import org.javamoney.moneta.Money;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import tourGuide.helper.InternalTestHelper;
 import tourGuide.model.*;
 import tourGuide.tracker.Tracker;
-import tourGuide.helper.InternalTestHelper;
-
 import tripPricer.Provider;
 import tripPricer.TripPricer;
 
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Service
 public class TourGuideService {
-    private Logger logger = LoggerFactory.getLogger(TourGuideService.class);
+    /**********************************************************************************
+     *
+     * Methods Below: For Internal Testing
+     *
+     **********************************************************************************/
+    private static final String tripPricerApiKey = "test-server-api-key";
+    public final Tracker tracker;
     private final GpsUtil gpsUtil;
     private final RewardsService rewardsService;
     private final TripPricer tripPricer = new TripPricer();
-    public final Tracker tracker;
+    // Database connection will be used for external users, but for testing purposes internal users are provided and stored in memory
+    private final Map<String, User> internalUserMap = new HashMap<>();
     boolean testMode = true;
+    private Logger logger = LoggerFactory.getLogger(TourGuideService.class);
     private List<UserReward> userRewards;
 
     public TourGuideService(GpsUtil gpsUtil, RewardsService rewardsService) {
@@ -100,8 +107,19 @@ public class TourGuideService {
         return providers;
     }
 
+    /*** OLD METHODE NOT USED
+     public List<Attraction> getNearByAttractions(VisitedLocation visitedLocation) {
+     List<Attraction> nearbyAttractions = new ArrayList<>();
+     for (Attraction attraction : gpsUtil.getAttractions()) {
+     if (rewardsService.isWithinAttractionProximity(attraction, visitedLocation.location)) {
+     nearbyAttractions.add(attraction);
+     }
+     }
+     return nearbyAttractions;
+     }
+     **/
+
     /**
-     *
      * @param user
      * @return visitedLocation
      */
@@ -138,13 +156,13 @@ public class TourGuideService {
 
         List<UserNearestAttractions> userNearestAttractionsList = attractions.parallelStream()
                 .map(a -> new UserNearestAttractions(
-                                a.attractionName,
-                                a.latitude,
-                                a.longitude,
-                                visitedLocation.location.latitude,
-                                visitedLocation.location.longitude,
-                                rewardsService.getDistance(a,visitedLocation.location),
-                                rewardsService.getRewardPoints(a,user)))
+                        a.attractionName,
+                        a.latitude,
+                        a.longitude,
+                        visitedLocation.location.latitude,
+                        visitedLocation.location.longitude,
+                        rewardsService.getDistance(a, visitedLocation.location),
+                        rewardsService.getRewardPoints(a, user)))
                 .sorted(Comparator.comparingDouble(UserNearestAttractions::getAttractionProximity))
                 .collect(Collectors.toList());
 
@@ -159,18 +177,60 @@ public class TourGuideService {
         return userNearestAttractionsResult;
     }
 
-    /*** OLD METHODE NOT USED
-    public List<Attraction> getNearByAttractions(VisitedLocation visitedLocation) {
-        List<Attraction> nearbyAttractions = new ArrayList<>();
-        for (Attraction attraction : gpsUtil.getAttractions()) {
-            if (rewardsService.isWithinAttractionProximity(attraction, visitedLocation.location)) {
-                nearbyAttractions.add(attraction);
-            }
-        }
-        return nearbyAttractions;
-    }
-    **/
+    // TODO : externaliser dans une classe de test
 
+    /**
+     * return the list of the user's preference
+     *
+     * @param userName
+     * @return the user's preference
+     */
+    public UserPreferencesDTO getUserPreference(String userName) {
+
+        User user = getUser(userName);
+
+        UserPreferencesDTO userPreferenceDTO = new UserPreferencesDTO(
+                user.getUserPreferences().getAttractionProximity(),
+                user.getUserPreferences().getCurrency().getCurrencyCode(),
+                user.getUserPreferences().getLowerPricePoint().getNumber().doubleValue(),
+                user.getUserPreferences().getHighPricePoint().getNumber().doubleValue(),
+                user.getUserPreferences().getTripDuration(),
+                user.getUserPreferences().getTicketQuantity(),
+                user.getUserPreferences().getNumberOfAdults(),
+                user.getUserPreferences().getNumberOfChildren(),
+                user.getUserPreferences().getNumberOfProposalAttraction()
+        );
+        return userPreferenceDTO;
+    }
+
+    /**
+     * update the user's preference
+     * @param userName
+     * @param userPreferences
+     * @return the user's preferences updated
+     */
+    public UserPreferences setUserPreference(String userName, UserPreferencesDTO userPreferences){
+
+        UserPreferences userPreferencesResult = new UserPreferences();
+        User user = getUser(userName);
+
+        CurrencyUnit currency = Monetary.getCurrency(userPreferences.getCurrency());
+        Money lowerPricePoint = Money.of(userPreferences.getLowerPricePoint(), currency);
+        Money highPricePoint = Money.of(userPreferences.getHighPricePoint(), currency);
+
+        userPreferencesResult.setAttractionProximity(userPreferences.getAttractionProximity());
+        userPreferencesResult.setCurrency(currency);
+        userPreferencesResult.setLowerPricePoint(lowerPricePoint);
+        userPreferencesResult.setHighPricePoint(highPricePoint);
+        userPreferencesResult.setTripDuration(userPreferences.getTripDuration());
+        userPreferencesResult.setTicketQuantity(userPreferences.getTicketQuantity());
+        userPreferencesResult.setNumberOfAdults(userPreferences.getNumberOfAdults());
+        userPreferencesResult.setNumberOfProposalAttraction(userPreferences.getNumberOfProposalAttraction());
+
+        user.setUserPreferences(userPreferencesResult);
+
+        return userPreferencesResult;
+    }
     private void addShutDownHook() {
         Runtime.getRuntime().addShutdownHook(new Thread() {
             public void run() {
@@ -178,16 +238,6 @@ public class TourGuideService {
             }
         });
     }
-
-    // TODO : externaliser dans une classe de test
-    /**********************************************************************************
-     *
-     * Methods Below: For Internal Testing
-     *
-     **********************************************************************************/
-    private static final String tripPricerApiKey = "test-server-api-key";
-    // Database connection will be used for external users, but for testing purposes internal users are provided and stored in memory
-    private final Map<String, User> internalUserMap = new HashMap<>();
 
     private void initializeInternalUsers() {
         IntStream.range(0, InternalTestHelper.getInternalUserNumber()).forEach(i -> {
